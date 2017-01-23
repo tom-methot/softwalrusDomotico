43715e6410c462a7181fa1381b56469b394c436c
Tommy Lepage-Méthot <tom.methot@gmail.com>
2017-01-23 15:43:19 -0500

    fix : call plugins

--- plugins/request/request.js
+++ plugins/request/request.js
@@ -7,20 +7,7 @@ exports.init = function(){
   // All declared variables are scoped to this plugin 
   
   info('Plugin REQUEST is initializing ...');
-  
-  var query = "126abzx";
-	var choices = [{id: 345, modelnumber: "123abc"},{id: 346, modelnumber: "123efg"},{id: 347, modelnumber: "456abdzx"}];
-	var options = {
-			scorer: fuzz.partial_ratio, // any function that takes two strings and returns a score, default: ratio
-			processor: function(choice) {return choice['modelnumber']},  //takes choice object, returns string, default: no processor. Must supply if choices are not already strings.
-			limit: 1, // max number of top results to return, default: no limit / 0.
-			cutoff: 70, // lowest score to return, default: 0
-			unsorted: false // results won't be sorted if true, default: false. If true limit will be ignored.
-	};
-
-	var results = fuzz.extract(query, choices, options);
-  
-  info('RESULT !!!!!!!!!!!!!!! : ' + results);
+  obj = require("./data.json");
 }
 
 exports.dispose = function(){
@@ -30,15 +17,6 @@ exports.dispose = function(){
   
   info('Plugin REQUEST is disposed ...');
 }
-
-exports.ajax = function(req, res, next){
-  // Called before rendering portlet when clicking on 
-  // <a href="/plugin/template" data-action="ajax">click me</a>  
-  // Because portlet CAN'T do asynchronous code
-  info('Plugin REQUEST ajax request ...');
-  next();
-}
-
 exports.speak = function(tts, async){
   // Hook called for each SARAH.speak()
   // to perform change on string
@@ -58,13 +36,10 @@ exports.standBy = function(motion, device){
 exports.action = function(data, next){
   // Called by SARAH to perform main action
   
-	obj = require("./data.json");
-	
-
+	var nextFunction;
 	var query = data.query;
 	info("query : " + query);
 	for(i in obj['plugins']){
-		info("obj " + obj['plugins'][i].name);
 		
 		var choices = obj['plugins'][i].items
 		var options = {
@@ -76,18 +51,22 @@ exports.action = function(data, next){
 
 		var results = fuzz.extract(query, choices, options);
 		if(results.length > 0){
-			info("res : " + results)
+			info("plugin : " + obj['plugins'][i].name + " search result : " + results);
 			info("CALL PLUGIN");
 			var e = SARAH.exists(obj['plugins'][i].name);
-			info(e);
+			info("plugins exist ? " + e);
 			if(e == true){
-				SARAH.run(obj['plugins'][i].name);
+				//SARAH.run(obj['plugins'][i].name);
+				SARAH.call(obj['plugins'][i].name, {}, function(options){ 
+					console.log(util.inspect(options, false, null));
+					nextFunction = options;
+				});
 			}
 			break;
 		}
 	}
   
-  next();
+  next(nextFunction);
 }
 
 
